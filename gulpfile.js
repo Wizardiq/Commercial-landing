f30103beb571813cd0db8de8e7f255b1fb04e50a
Vlad Maksimov <vmaksimov.dev@gmail.com>
2020-05-08 22:49:21 +0300

    fix semantic

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp'),
- less = require('gulp-less'),
- browserSync = require('browser-sync'),
-  plumber      = require('gulp-plumber'),
-   rename       = require('gulp-rename'),
-   cleanCSS     = require('gulp-clean-css');
-   autoprefixer = require('gulp-autoprefixer');
+var gulp  = require('gulp'),
+    less  = require('gulp-less'),
+    browserSync  = require('browser-sync'),
+    plumber      = require('gulp-plumber'),
+    rename       = require('gulp-rename'),
+    cleanCSS     = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('less', function(){ // Создаем таск "less"
-return gulp.src('less/style.less') // Берем источник
+gulp.task('less', function(){
+return gulp.src('less/style.less') 
 .pipe(plumber())
 .pipe(less())
 .pipe(cleanCSS())
@@ -18,28 +18,25 @@ return gulp.src('less/style.less') // Берем источник
 }))
 .pipe(autoprefixer({overrideBrowserslist: ['last 5 versions']}))
 .pipe(cleanCSS({compatibility: 'ie10', keepSpecialComments: 1}))
- // Преобразуем Sass в CSS посредством gulp-less
-    .pipe(gulp.dest('css')) // Выгружаем результата в папку 
+    .pipe(gulp.dest('css')) 
     .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-  browserSync({ // Выполняем browserSync
-      server: { // Определяем параметры сервера
-          baseDir: './' // Директория для сервера 
+gulp.task('browser-sync', function() { 
+  browserSync({ 
+      server: { 
+          baseDir: './' 
       },
-      notify: false // Отключаем уведомления
+      notify: false 
   });
 });
 
-// Watch
+
 gulp.task('watch',  function() {
   gulp.watch('less/**/*.less', gulp.parallel('less'));
     gulp.watch(['index.html']);
     gulp.watch(['img/**/*']);
-   // Наблюдение за другими типами файлов
 });
 
-// Default task
 
 gulp.task('default', gulp.parallel("less", 'browser-sync', 'watch'));
